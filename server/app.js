c633eccb85eb731e385f356acd1826086c8a74b2
ChanceYu <i.fish@foxmail.com>
2018-10-17 21:21:45 +0800

    change update schedules

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ later.date.localTime()
 later.setInterval(handlerUpdate, {
   schedules: [
     { h: [06], m: [00] },
-    { h: [10], m: [00] },
+    { h: [08], m: [00] },
     { h: [12], m: [00] },
     { h: [18], m: [00] },
     { h: [22], m: [00] },
