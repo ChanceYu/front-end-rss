adc337823dac98ceaa4fd1021861495f61355303
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-16 17:01:02 +0800

    Delete site directory

--- site/build/data.js
+++
@@ -1,9 +0,0 @@
-const RSS_DATA = require('../../data/rss.json')
-const TAGS_DATA = require('../../data/tags.json')
-const LINKS_DATA = require('../../data/links.json')
-
-module.exports = {
-  RSS_DATA,
-  TAGS_DATA,
-  LINKS_DATA
-}
--- site/build/template-parameters.js
+++
@@ -1,9 +0,0 @@
-const data = require('./data')
-
-const { RSS_DATA, TAGS_DATA, LINKS_DATA } = data
-
-module.exports = {
-  RSS_DATA: JSON.stringify(RSS_DATA),
-  TAGS_DATA: JSON.stringify(TAGS_DATA),
-  LINKS_DATA: JSON.stringify(LINKS_DATA)
-}
